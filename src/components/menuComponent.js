a1564e5379eb7f70e30845a87ea7dda7fbdd1079
Muhammad Ammar Bashir <myselfammar@gmail.com>
2021-08-03 16:56:35 +0500

    Add Components files

---
+++ src/components/menuComponent.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { Card, CardTitle, CardBody, CardImgOverlay, CardImg, CardText } from 'reactstrap';
+
+class Menu extends Component{
+
+    constructor (props){
+        super(props);
+
+            this.state = {
+                selectedDish: null
+                
+            };
+    }
+
+    onDishSelect(dish){
+        this.setState({ selectedDish : dish })
+    }
+
+    randerDish(dish){
+        if(dish != null){
+            return(
+                <Card>
+                    <CardImg width="100%" src={dish.image} />
+                    <CardTitle> {dish.name} </CardTitle>
+                    <CardText> {dish.description} </CardText>
+                    <CardText> {dish.price} </CardText>
+                </Card>
+            );
+
+        }
+
+        else{
+            <div> </div>
+
+        }
+
+    }
+
+
+    render(){
+
+        const menu = this.props.dishes.map((dish) => {
+            return(
+                <div key={dish.id} className= "col-12 col-md-5 m-1">
+                    <Card onClick={()=> this.onDishSelect(dish) }>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImgOverlay>
+                            <CardTitle> {dish.name} </CardTitle>
+                        </CardImgOverlay>
+            
+                    </Card>
+                </div>
+            );
+        }); 
+
+        return(
+            <div className="container">
+                <div className="row">
+                    {menu}
+                </div>
+                <div className="row">
+                    {this.randerDish(this.state.selectedDish)}
+                </div>
+            </div>
+
+        );
+    }
+}
+
+
+export default Menu;
